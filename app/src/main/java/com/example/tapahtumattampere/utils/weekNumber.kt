78dd065181d9081e736bdb65670db7cb383bb755
Raine Tikkanen <raine.tikkan3n@gmail.com>
2025-01-18 11:30:57 +0200

    test

--- app/src/main/java/com/example/tapahtumattampere/utils/weekNumber.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tapahtumattampere.utils
-
-import java.time.LocalDateTime
-import java.time.temporal.ChronoField
-
-fun LocalDateTime.getWeekNumber(): Int {
-    return get(ChronoField.ALIGNED_WEEK_OF_YEAR)
-}
